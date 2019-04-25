6408856854bd4a8ab886dd4993c31a2855ac710e
Andrey Lapin <lapinandrey95@gmail.com>
2019-04-24 23:46:46 -0400

    Created login page

--- src/user/models.py
+++ src/user/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
-class User(models.Model):
-    first_name  = models.CharField(max_length=120)
-    last_name   = models.CharField(max_length=120, null=True, blank=True)
+class Employee(models.Model):
+    owner           = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name      = models.CharField(max_length=120)
+    last_name       = models.CharField(max_length=120, null=True, blank=True)
+    birthday        = models.DateField(null=True, blank=True)
+    phone_number    = models.CharField(max_length=120, null=True, blank=True)
+    address         = models.CharField(max_length=120, null=True, blank=True)
--- src/user/views.py
+++ src/user/views.py
@@ -1,14 +1,31 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.contrib.auth.views import LoginView
+from django.db.models import Q
+from django.urls import reverse
 
-from .models import User
+from .models import Employee
 
 # Create your views here.
 
-def user_listview(request):
-    template_name = "user/user.html"
-    queryset = User.objects.all()
-    context = {
-        "object_list": queryset
-    }
-    return render(request, template_name, context)
+class EmployeeListView(LoginRequiredMixin, ListView):
+
+    login_url = "/login/"
+
+    def get_queryset(self):
+        queryset = Employee.objects.none()
+        try:
+            owner = User.objects.get(username=self.request.user).id
+            user_id = Employee.objects.get(owner_id=owner).id
+        except:
+            return queryset
+
+        if user_id is not None:
+            queryset = Employee.objects.filter(
+                Q(id=user_id)
+            )
+
+        return queryset
