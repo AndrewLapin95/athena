200d4026d68a9fa15b4d090795dabb11324445ff
Andrey Lapin <lapinandrey95@gmail.com>
2019-04-30 00:57:21 -0400

    Refactored code

--- src/user/backends.py
+++ src/user/backends.py
@@ -7,26 +7,27 @@ from user.models import UserProfile
 class CustomAuthenticationBackend:
     
     def authenticate(self, request, username=None, password=None, **kwargs):
-        
-        form_alias = request.POST.get("alias")
-        if form_alias == "" or form_alias is None:
-            return None
-
         try:
             user = User.objects.get(username=username)
-            alias = UserProfile.objects.get(user_id=user.id).company_alias
-            if getattr(user, 'is_active') and user.check_password(password) and alias == form_alias:
-                return user
         except User.DoesNotExist:
             try:
                 user = User.objects.get(email=username)
-                alias = UserProfile.objects.get(user_id=user.id).company_alias
-                if getattr(user, 'is_active') and user.check_password(password) and alias == form_alias:
-                    return user
             except User.MultipleObjectsReturned:
                 return None
             except User.DoesNotExist:
                 return None
+
+        if not user.is_superuser:
+                form_alias = request.POST.get("alias")
+                if form_alias == "" or form_alias is None:
+                    return None
+
+                alias = UserProfile.objects.get(user_id=user.id).company_alias
+                if getattr(user, 'is_active') and user.check_password(password) and alias == form_alias:
+                    return user
+        else:
+            if getattr(user, 'is_active') and user.check_password(password):
+                return user
 
         return None
 
