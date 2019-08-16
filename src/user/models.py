29ba0436a4462a15ba29d0e6ce6d4fe4c7fa4a40
Andrey Lapin <lapinandrey95@gmail.com>
2019-08-16 00:30:09 -0400

    Updated user model

--- src/user/models.py
+++ src/user/models.py
@@ -3,12 +3,9 @@ from django.conf import settings
 
 USER = settings.AUTH_USER_MODEL
 
-EMPLOYEE = "EM"
-MANAGER = "HR"
-
-ROLE_CHOICES = (
-    (EMPLOYEE, 'Employee'),
-    (MANAGER, 'Manager'),
+USER_TYPES = (
+    (1, "employee"),
+    (2, "manager")
 )
 
 class UserProfile(models.Model):
@@ -16,13 +13,13 @@ class UserProfile(models.Model):
     User profile class. Adds role and company role to the user field
     """
     user = models.OneToOneField(USER, on_delete=models.CASCADE)
-    role = models.CharField(default=EMPLOYEE, max_length=2, choices=ROLE_CHOICES)
+    role = models.PositiveSmallIntegerField(default=1, choices=USER_TYPES)
     company_alias = models.CharField(max_length=10, null=False, blank=False)
 
 # Create your models here.
 class Employee(models.Model):
     """
-    Employee class (will be removed)
+    Employee model class
     """
     owner = models.ForeignKey(USER, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=120)
@@ -30,3 +27,19 @@ class Employee(models.Model):
     birthday = models.DateField(null=True, blank=True)
     phone_number = models.CharField(max_length=120, null=True, blank=True)
     address = models.CharField(max_length=120, null=True, blank=True)
+    team = models.CharField(max_length=120, null=True, blank=True)
+    date_of_joining = models.DateField()
+    email = models.CharField(max_length=120, null=True, blank=True)
+    gender = models.CharField(max_length=120, null=True, blank=True)
+    manager = models.CharField(max_length=120, null=True, blank=True)
+
+class EmergencyContact(models.Model):
+    """
+    Emergency contact model class
+    """
+    owner = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+    relationship = models.CharField(max_length=120)
+    birthday = models.DateField(null=False, blank=False)
+    phone_number = models.CharField(max_length=120, null=False, blank=False)
+    
